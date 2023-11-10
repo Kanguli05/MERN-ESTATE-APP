31361d79ab71da98c74589f0e49a9d7f1ee9d4f3
Kanguli05 <willykanguli@gmail.com>
2023-11-10 15:33:46 +0300

    Render home page on App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
