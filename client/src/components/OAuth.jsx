5edd21e1ded756e2a8275b9d95a3c799adc98896
Kanguli05 <willykanguli@gmail.com>
2024-04-19 04:15:45 +0300

    Add onChange and onSubmit functionality to the search page

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -20,16 +20,16 @@ export default function OAuth() {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
-            })
+            });
             const data = await res.json()
             dispatch(signInSuccess(data));
             navigate('/');
         } catch (error) {
             console.log('Could not sign in with Google', error);
         }
-    }
+    };
 
   return (
     <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
-  )
+  );
 }
