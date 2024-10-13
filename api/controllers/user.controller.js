da7d45b2ee7afbce1308e83b8a2b356e174eb652
Kanguli05 <willykanguli@gmail.com>
2024-10-13 10:39:03 +0300

    Minor changes to package.json

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,7 +29,7 @@ export const updateUser =  async (req, res, next) => {
 
         const {password, ...others} = updatedUser._doc;
 
-        res.status(200).json(rest);
+        res.status(200).json("User has been updated!");
     } catch (error) {
         next(error)
     }
@@ -37,7 +37,7 @@ export const updateUser =  async (req, res, next) => {
 
 
 export const deleteUser = async (req, res, next ) => {
-    if (req.user.id !==req.params.id) return next(errorHandler(401, 'You can only delete your own account!'));
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'));
     try {
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie('access_token');
